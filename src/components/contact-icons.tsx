b3ae08167b4572ed1ae1d1b90d1c85d243d22de4
Asahi Takenouchi <le-soleil-levant.0802@outlook.com>
2020-10-30 23:29:03 +0900

    fix: sns icons

--- src/components/contact-icons.tsx
+++ src/components/contact-icons.tsx
@@ -5,41 +5,31 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { faTwitter, faFacebook, faInstagram, faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 import { faEnvelope } from '@fortawesome/free-regular-svg-icons'
 
-const ContactIcons: React.FC = () => {
-  // Load necessary icons
-  library.add(faTwitter)
-  library.add(faFacebook)
-  library.add(faInstagram)
-  library.add(faGithub)
-  library.add(faLinkedin)
-  library.add(faEnvelope)
-
-  return (
-    <div
-      sx={{
-        a: { color: `text`, fontSize: 4, mr: 2 },
-      }}
-    >
-      <Styled.a href={`https://twitter.com/ashtkn`}>
-        <FontAwesomeIcon icon={[`fab`, `twitter`]} />
-      </Styled.a>
-      <Styled.a href={`https://www.facebook.com/asahi.taken`}>
-        <FontAwesomeIcon icon={[`fab`, `facebook`]} />
-      </Styled.a>
-      <Styled.a href={`https://www.instagram.com/ash.take`}>
-        <FontAwesomeIcon icon={[`fab`, `instagram`]} />
-      </Styled.a>
-      <Styled.a href={`https://github.com/ashtkn`}>
-        <FontAwesomeIcon icon={[`fab`, `github`]} />
-      </Styled.a>
-      <Styled.a href={`https://www.linkedin.com/in/ashtkn`}>
-        <FontAwesomeIcon icon={[`fab`, `linkedin`]} />
-      </Styled.a>
-      <Styled.a href={`mailto:asahi.taken@gmail.com`}>
-        <FontAwesomeIcon icon={[`far`, `envelope`]} />
-      </Styled.a>
-    </div>
-  )
-}
+const ContactIcons: React.FC = () => (
+  <div
+    sx={{
+      a: { color: `text`, fontSize: 4, mr: 2 },
+    }}
+  >
+    <Styled.a href={`https://twitter.com/ashtkn`}>
+      <FontAwesomeIcon icon={faTwitter} />
+    </Styled.a>
+    <Styled.a href={`https://www.facebook.com/asahi.taken`}>
+      <FontAwesomeIcon icon={faFacebook} />
+    </Styled.a>
+    <Styled.a href={`https://www.instagram.com/ash.take`}>
+      <FontAwesomeIcon icon={faInstagram} />
+    </Styled.a>
+    <Styled.a href={`https://github.com/ashtkn`}>
+      <FontAwesomeIcon icon={faGithub} />
+    </Styled.a>
+    <Styled.a href={`https://www.linkedin.com/in/ashtkn`}>
+      <FontAwesomeIcon icon={faLinkedin} />
+    </Styled.a>
+    <Styled.a href={`mailto:asahi.taken@gmail.com`}>
+      <FontAwesomeIcon icon={faEnvelope} />
+    </Styled.a>
+  </div>
+)
 
 export default ContactIcons
