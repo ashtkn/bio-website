aac3a91eec62afbe9b9f6e11df9f30b5e59a3b0c
Asahi Takenouchi <le-soleil-levant.0802@outlook.com>
2020-10-31 14:43:10 +0900

    feat: add a project

--- src/components/project-list/caption.tsx
+++ src/components/project-list/caption.tsx
@@ -1,4 +1,5 @@
-import { faYoutube } from '@fortawesome/free-brands-svg-icons'
+import { faGithub, faYoutube } from '@fortawesome/free-brands-svg-icons'
+import { faLink } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
 import { Styled } from 'theme-ui'
@@ -6,17 +7,33 @@ import { Styled } from 'theme-ui'
 type CaptionProps = {
   title: string
   technology: string
+  website?: string
   youtube?: string
+  github?: string
 }
 
-const Caption: React.FC<CaptionProps> = ({ title, youtube }) => (
+const Caption: React.FC<CaptionProps> = ({ title, website, youtube, github }) => (
   <div>
     <Styled.p className="text-lg">{title}</Styled.p>
+    {website ? (
+      <Styled.a href={website}>
+        <FontAwesomeIcon icon={faLink} />
+        {` `}
+        <span>Website{`  `}</span>
+      </Styled.a>
+    ) : null}
     {youtube ? (
       <Styled.a href={youtube}>
         <FontAwesomeIcon icon={faYoutube} />
         {` `}
-        <span>YouTube</span>
+        <span>YouTube{`  `}</span>
+      </Styled.a>
+    ) : null}
+    {github ? (
+      <Styled.a href={github}>
+        <FontAwesomeIcon icon={faGithub} />
+        {` `}
+        <span>GitHub{`  `}</span>
       </Styled.a>
     ) : null}
   </div>
